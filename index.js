cd8b2e32b6e2ef5b0041a3ca0f5ee1f0b9ca1dab
Hatim-Gw <m09181014@it.misuratau.edu.ly>
2026-04-18 10:02:30 +0200

    html pages

--- index.js
+++ index.js
@@ -4,7 +4,26 @@ const fs = require("fs");
 
 const httpServer = http.createServer((req, res) => {
   if (req.url === "/") {
-    res.end("hello world");
+    fs.readFile(
+      path.join(__dirname, "public", "index.html"),
+      (err, content) => {
+        if (err) throw err;
+
+        res.setHeader("Content-Type", "text/html");
+        res.end(content);
+      },
+    );
+  }
+  if (req.url === "/about") {
+    fs.readFile(
+      path.join(__dirname, "public", "about.html"),
+      (err, content) => {
+        if (err) throw err;
+
+        res.setHeader("Content-Type", "text/html");
+        res.end(content);
+      },
+    );
   }
 });
 
